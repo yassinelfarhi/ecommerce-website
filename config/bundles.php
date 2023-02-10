76dafbb4525946a5b8bea2b322698f051fd00c9c
soufian <elhabib.soufian@gmail.com>
2023-02-10 22:36:11 +0100

    add slug command

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
