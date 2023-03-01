4f705264bd15e5bbbb830f664e81bca52056ae03
Soufian <selhabib@ze-camping.com>
2023-03-01 07:38:04 +0100

    hover

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Product;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -24,7 +29,16 @@ class AdminDashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-dashboard');
+        yield MenuItem::linkToCrud('Product', 'fas fa-box', Product::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-users',  User::class);
     }
+
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+                ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
+
 }
--- src/Controller/BackEnd/BackEndController.php
+++ src/Controller/BackEnd/BackEndController.php
@@ -5,6 +5,7 @@ namespace App\Controller\BackEnd;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class BackEndController extends AbstractController
 {
